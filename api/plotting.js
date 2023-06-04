7d4575191ae3ed8a129fd08d73223bef0244bbc4
MichaelWilsonn <135476969+MichaelWilsonn@users.noreply.github.com>
2023-06-03 17:12:36 -0700

    Create plotting.js

---
+++ api/plotting.js
@@ -0,0 +1,16 @@
+const plotBarGraph = (data) => {
+  // Add code to generate bar graph based on the data
+  // Return the bar graph image or data in the desired format
+};
+
+const plotLineGraph = (data) => {
+  // Add code to generate line graph based on the data
+  // Return the line graph image or data in the desired format
+};
+
+const plotScatterPlot = (data) => {
+  // Add code to generate scatter plot based on the data
+  // Return the scatter plot image or data in the desired format
+};
+
+module.exports = { plotBarGraph, plotLineGraph, plotScatterPlot };
